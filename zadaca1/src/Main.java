d17b8d748d5f45968ec078e5ad665211270bf856
Aldin Rizvo <aldin_rizvo18@hotmail.com>
2023-03-18 16:32:31 +0100

    Homework 1 solution

---
+++ zadaca1/src/Main.java
@@ -0,0 +1,125 @@
+public class Main {
+    public static void main(String[] args) {
+        String nameAndSurname = "Aldin Rizvo";
+        int birthYear = 1996;
+        char nameFirstLetter = 'A';
+        long largeNumber = 100000000000L;
+        double decimalNumber = 42.001;
+        boolean isFortyTwoAnswer = true;
+        String textMessage = "Answer to the Ultimate Question of Life, the Universe, and Everything is the number 42: " + isFortyTwoAnswer;
+        double originalNumber = 100.78;
+        int numberOfDoubling = 1;
+        String doublingCounter = "X";
+    /*
+        int randomInt = 66;
+        long randomLong = 200L;
+        float randomFloat = 10.7F;
+        double randomDouble = 200.75;
+        char randomChar = 'A';
+        String someText = "Some random text";
+        boolean trueOrFalse = true;
+
+    //    randomInt = randomLong;   // incompatible types
+    //    randomInt = randomFloat;  // incompatible types
+    //    randomInt = randomDouble; // incompatible types
+        randomInt = randomChar;     // ASCII value of char 'A' assigned to int variable
+        System.out.println("Char variable '" + randomChar + "' assigned to int variable: " + randomInt + "\n");
+    //    randomInt = someText;     // incompatible types
+    //    randomInt = trueOrFalse;  // incompatible types
+
+        randomLong = randomInt;
+        System.out.println("Int variable " + randomInt +" assigned to long variable: " + randomLong);
+    //    randomLong = randomFloat; // incompatible types
+    //    randomLong = randomDouble;// incompatible types
+        randomLong = randomChar;
+        System.out.println("Char variable '" + randomChar + "' assigned to long variable: " + randomLong + "\n");
+    //    randomLong = someText;    // incompatible types
+    //    randomLong = trueOrFalse; // incompatible types
+
+        randomFloat = randomInt;
+        System.out.println("Int variable " + randomInt + " assigned to float variable: " + randomFloat);
+        randomFloat = randomLong;
+        System.out.println("Long variable " + randomLong + " assigned to float variable: " + randomFloat);
+    //    randomFloat = randomDouble;// incompatible types
+        randomFloat = randomChar;
+        System.out.println("Char variable '" + randomChar + "' assigned to float variable: " + randomFloat + "\n");
+    //    randomFloat = someText;    // incompatible types
+    //    randomFloat = trueOrFalse; // incompatible types
+
+        randomDouble = randomInt;
+        System.out.println("Int variable " + randomInt + " assigned to double variable: " + randomDouble);
+        randomDouble = randomLong;
+        System.out.println("Long variable " + randomLong + " assigned to double variable: " + randomDouble);
+        randomDouble = randomFloat;
+        System.out.println("Float variable " + randomFloat + " assigned to double variable: " + randomDouble);
+        randomDouble = randomChar;
+        System.out.println("Char variable '" + randomChar + "' assigned to double variable: " + randomDouble + "\n");
+    //    randomDouble = someText;  // incompatible types
+    //    randomDouble = trueOrFalse; // incompatible types
+
+    //    randomChar = randomInt;    // incompatible types
+    //    randomChar = randomLong;   // incompatible types
+    //    randomChar = randomFloat;  // incompatible types
+    //    randomChar = randomDouble; // incompatible types
+    //    randomChar = someText;     // incompatible types
+    //    randomChar = trueOrFalse;  // incompatible types
+
+    //    someText = randomInt;      // incompatible types
+    //    someText = randomLong;     // incompatible types
+    //    someText = randomFloat;    // incompatible types
+    //    someText = randomDouble;   // incompatible types
+    //    someText = randomChar;     // incompatible types
+    //    someText = trueOrFalse;    // incompatible types
+
+    //    trueOrFalse = randomInt;   // incompatible types
+    //    trueOrFalse = randomLong;  // incompatible types
+    //    trueOrFalse = randomFloat; // incompatible types
+    //    trueOrFalse = randomDouble;// incompatible types
+    //    trueOrFalse = randomChar;  // incompatible types
+    //    trueOrFalse = someText;    // incompatible types
+    */
+
+        System.out.print((char)65 + "" + (char)108 + "" + (char)100 + "" + (char)105 + "" + (char)110 + "\n\n");
+
+        System.out.println(textMessage + "\n");
+
+        System.out.println("This is a number " + originalNumber + " and it is about to double.");
+        originalNumber = originalNumber * 2;
+        System.out.println("This is a number " + originalNumber + " and it is about to double. The original number has been doubled: " + doublingCounter + " (Total of: " + numberOfDoubling + " times).");
+
+        originalNumber = originalNumber * 2;
+        doublingCounter = doublingCounter + "X";
+        numberOfDoubling = numberOfDoubling + 1;
+        System.out.println("This is a number " + originalNumber + " and it is about to double. The original number has been doubled: " + doublingCounter + " (Total of: " + numberOfDoubling + " times).");
+
+        originalNumber = originalNumber * 2;
+        doublingCounter = doublingCounter + "X";
+        numberOfDoubling = numberOfDoubling + 1;
+        System.out.println("This is a number " + originalNumber + " and it is about to double. The original number has been doubled: " + doublingCounter + " (Total of: " + numberOfDoubling + " times).");
+
+        originalNumber = originalNumber * 2;
+        doublingCounter = doublingCounter + "X";
+        numberOfDoubling = numberOfDoubling + 1;
+        System.out.println("This is a number " + originalNumber + " and it is about to double. The original number has been doubled: " + doublingCounter + " (Total of: " + numberOfDoubling + " times).");
+
+        originalNumber = originalNumber * 2;
+        doublingCounter = doublingCounter + "X";
+        numberOfDoubling = numberOfDoubling + 1;
+        System.out.println("This is a number " + originalNumber + " and it is about to double. The original number has been doubled: " + doublingCounter + " (Total of: " + numberOfDoubling + " times).");
+
+        originalNumber = originalNumber * 2;
+        doublingCounter = doublingCounter + "X";
+        numberOfDoubling = numberOfDoubling + 1;
+        System.out.println("This is a number " + originalNumber + " and it is about to double. The original number has been doubled: " + doublingCounter + " (Total of: " + numberOfDoubling + " times).");
+
+        originalNumber = originalNumber * 2;
+        doublingCounter = doublingCounter + "X";
+        numberOfDoubling = numberOfDoubling + 1;
+        System.out.println("This is a number " + originalNumber + " and it is about to double. The original number has been doubled: " + doublingCounter + " (Total of: " + numberOfDoubling + " times).");
+
+        originalNumber = originalNumber * 2;
+        doublingCounter = doublingCounter + "X";
+        numberOfDoubling = numberOfDoubling + 1;
+        System.out.println("This is a number " + originalNumber + " and it is about to double. The original number has been doubled: " + doublingCounter + " (Total of: " + numberOfDoubling + " times).");
+    }
+}
